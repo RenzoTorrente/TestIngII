7605d21976faf5c3daa9e3a3e380fde68a8ae764
RenzoTorrente <renzotor7@hotmail.com>
2021-11-02 00:50:22 -0300

    empleado eventual renzo ferronato verificado

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -3,7 +3,7 @@ require_once "EmpleadoTest.php";
 
 class EmpleadoEventualTest extends EmpleadoTest
 {
-	public function crear($nombre="Renzo",$apellido="Ferronato",$dni="44231749",$salario="9000",$array = array(20, 10, 40,20))
+	public function crear($nombre="Renzo",$apellido="Torrente",$dni="40843382",$salario="90000",$array = array(20, 10, 40,20))
 	{
 		$r = new \App\EmpleadoEventual($nombre,$apellido,$dni,$salario,$array);
 		return $r;
@@ -18,7 +18,7 @@ class EmpleadoEventualTest extends EmpleadoTest
     public function testMetodoCalcularIngresoTotal()
     {
         $r = $this->crear();
-        $this->assertEquals(90001.125, $r->calcularIngresoTotal());
+        $this->assertEquals( 90001.125, $r->calcularIngresoTotal());
     }
 
     public function testMontoDeVentaNegativoOCero()
